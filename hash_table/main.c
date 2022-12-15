313ce1dfe7fc2a7075996562de5aa73fbdde8d32
Yotam <yotamsoudry@gmail.com>
2022-12-15 17:41:40 +0200

    securing the shell

--- hash_table/main.c
+++ hash_table/main.c
@@ -23,35 +23,25 @@ int main(void){
     strcpy(quote, "Lightning bolts shoot from my fingertips!");
     insert_item(hash_table, name, bday, quote);
 
-    printf("\n");
-
     strcpy(name, "Walter White");
     strcpy(bday, "7/9/1958");
     strcpy(quote, "I am the danger. I am the one who knocks");
     insert_item(hash_table, name, bday, quote);
-
-    printf("\n");
 
     strcpy(name, "Saul Goodman");
     strcpy(bday, "12/11/1960");
     strcpy(quote, "I'm like a god in human clothing");
     insert_item(hash_table, name, bday, quote);
 
-    puts("");
-
     strcpy(name, "Gustavo Fring");
     strcpy(bday, "6/9/1958");
     strcpy(quote, "I will kill your wife. I will kill your son. I will kill your infant daughter");
     insert_item(hash_table, name, bday, quote);
 
-    puts("");
-
     strcpy(name, "Peter Griffin");
     strcpy(bday, "10/4/1975");
     strcpy(quote, "Holy crap Lois I'm India!");
     insert_item(hash_table, name, bday, quote);
-
-    puts("");
 
     strcpy(name, "Dr. Michael Morbius");
     strcpy(bday, "1/4/2022");
@@ -60,7 +50,6 @@ int main(void){
 
     clock_t end = clock();
     double time = (double)(end - begin)/CLOCKS_PER_SEC;
-    printf("Time Elapsed: %f\n", time);
 
     free(name);
     free(bday);
@@ -70,36 +59,45 @@ int main(void){
     char *line;
     size_t buffsize = 155;
     size_t len;
-
-    const char space[] = " ";
     
     system("cls");
-    
     while (1 == 1){ 
         printf("ht> ");
         int code = read_input(line, buffsize); // success/error code after reading the input
         if (code == -1) continue;
 
-        if (strcmp(line, "exit") == 0) break;
+        if (strcmp(line, "exit") == 0 || strcmp(line, "quit") == 0) break;
         if (strcmp(line, "cls") == 0 || strcmp(line, "clear") == 0){
             system("cls");
+            continue;
+        } else if (strcmp(line, "help") == 0){
+            shell_print_help();
+            continue;
         }
         
-        char *word = strtok(line, space);
+        char *command = strtok(line, " ");
 
         char *tmp = strtok(NULL, "\n");
-        if (tmp) line = tmp; else continue; //checking that 'line' doesn't get a null value
+        if (tmp) {
+            line = tmp; 
+        }
+        else {
+            printf("Invalid command. Enter 'help' for info\n");
+            continue; // making sure 'line' doesn't get a null value
+        }
 
-        if (strcmp(word, "insert") == 0) {
+        if (strcmp(command, "insert") == 0) {
             shell_insert(line, hash_table);
-        } else if (strcmp(word, "remove") == 0) {
+        } else if (strcmp(command, "remove") == 0) {
             shell_remove(line, hash_table);
-        } else if (strcmp(word, "update") == 0){
+        } else if (strcmp(command, "update") == 0){
             shell_update(line, hash_table);
-        } else if (strcmp(word, "print") == 0) {
+        } else if (strcmp(command, "print") == 0) {
             shell_print(line, hash_table);
-        } else if (strcmp(word, "hash") == 0) {
+        } else if (strcmp(command, "hash") == 0) {
             shell_print_hash(line);
+        } else {
+            printf("Invalid command. Enter 'help' for info\n");
         }
     } 
 
--- hash_table/shell.h
+++ hash_table/shell.h
@@ -9,62 +9,85 @@
 #define NAME_LENGTH 30
 #define BDAY_LENGTH 11
 #define QUOTE_LENGTH 100
+const char *seperator = ";";
 //TODO: secure the inputs (buffer overflow, injection...)
 
-const char semicolon[] = ";";
-const char newline[] = "\n";
-
-void shell_insert(char *input, item **ht){
-    char *name = strtok(input, semicolon);
-    if(strlen(name) > NAME_LENGTH - 1){
-        puts("Name too long, failed to insert");
-        return;
+int validate_input(char *input, const char *key, int maxlen, char *field){
+    int i = 0;
+    char k = key[0];
+    if (input == NULL) {
+        printf("Missing fields. Enter 'help' for info\n");
+        return -1;
     }
 
-    char *bday = strtok(NULL, semicolon);
-    if (strlen(bday) > BDAY_LENGTH - 1){
-        puts("Bday too long, failed to insert");
-        return;
-    }
+    while (input[0] == ' ') input++;
+    for (; i <= maxlen; i++){
+        if (input[i] == k) {
+            if (i == 0){
+                printf("Invalid input. Enter 'help' for info\n");
+                return -1;
+            }
+            return 0;
+        }
+        if (input[i] == '\0') {
+            printf("Missing fields. Enter 'help' for info\n");
+            return -1;
+        }
 
-    char *quote = strtok(NULL, newline);
-    if (strlen(quote) > QUOTE_LENGTH - 1){
-        puts("Quote too long, failed to insert");
-        return;
+        if (!((input[i] <= 'z' && input[i] >= 'a') || (input[i] <= 'Z' && input[i] >= 'A') || input[i] == ' ')){
+            printf("Invalid character at position %d => %c\n", i, input[i]); // Making sure all chars are alphabetic and spaces
+            return -1;
+        }
+        if (input[i] == ' '){
+            if (input[i+1] == k || input[i+1] == ' ')
+            {
+                printf("Invalid character at position %d => space cannot be the last char in the string", i);
+                printf("and cannot be followed by another space\n");
+                return -1;
+            }
+        }
     }
+    printf("Invalid input. %s must be %d chars long, followed by a '%c'\n", field, maxlen, k);
+    return -1;
+}
+
+void shell_insert(char *input, item **ht){    
+    if (validate_input(input, seperator, NAME_LENGTH, "name") == -1) return;
+    char *name = strtok(input, seperator);
+
+    char *bday = strtok(NULL, "");
+    if (validate_input(bday, seperator, BDAY_LENGTH, "bday") == -1) return;
+    bday = strtok(bday, seperator);
+
+    char *quote = strtok(NULL, "");
+    if (validate_input(quote, "\n", QUOTE_LENGTH, "quote") == -1) return;
+    quote = strtok(NULL, "\n");
 
     insert_item(ht, name, bday, quote);
 }
 
 void shell_print(char *input, item **ht){
-    char *name = strtok(input, newline);
+    char *name = input;
     print_item(ht, name);
 }
 
 void shell_remove(char *input, item **ht){
-    char *name = strtok(input, newline);
+    char *name = input;
     remove_item(ht, name);
 }
 
 void shell_update(char *input, item **ht){
-    char *name = strtok(input, semicolon);
-    if(strlen(name) > NAME_LENGTH - 1){
-        puts("Name too long, failed to insert");
-        return;
-    }
+    if (validate_input(input, seperator, NAME_LENGTH, "name") == -1) return;
+    char *name = strtok(input, seperator);
 
-    char *bday = strtok(NULL, semicolon);
-    if (strlen(bday) > BDAY_LENGTH - 1){
-        puts("Bday too long, failed to insert");
-        return;
-    }
+    char *bday = strtok(NULL, "");
+    if (validate_input(bday, seperator, BDAY_LENGTH, "bday") == -1) return;
+    bday = strtok(bday, seperator);
 
-    char *quote = strtok(NULL, newline);
-    if (strlen(quote) > QUOTE_LENGTH - 1){
-        puts("Quote too long, failed to insert");
-        return;
-    }
-    
+    char *quote = strtok(NULL, "");
+    if (validate_input(quote, "\n", QUOTE_LENGTH, "quote") == -1) return;
+    quote = strtok(NULL, "\n");
+
     update_item(ht, new_item(name, bday, quote));
 }
 
@@ -72,19 +95,44 @@ int read_input(char *buff, int buffsize){
     int pos = 0;
     while (1){
         buff[pos] = getc(stdin);
-        if (buff[pos] == '\n' || buff[pos] == EOF || buff[pos] == '\0'){
-            buff[pos] = '\0';
+        if (buff[pos] == '\n'){
+            buff[pos] = '\0';  // End input on newline char
             return 0;
         }
         pos++;
         if (pos > buffsize){
-            puts("Input too large");
+            puts("Input too large. Enter 'help' for info");
             return -1;
         }
     }
 }
 
-void shell_print_hash(char *name){
-    unsigned short i = hash_function(name, HT_BASE_SIZE);
-    printf("%s:\t%d\n", name, i);
+void shell_print_hash(char *input){
+    unsigned short i = hash_function(input, HT_BASE_SIZE);
+    printf("%s:\t%d\n", input, i);
+}
+
+void shell_print_help(){
+    printf("\n\tCreate new entry in the hash table:\n");
+    printf("\t\tinsert <name (max %d chars)>; <bday (max %d chars)>; <quote (max %d chars)>\n\n", 
+        NAME_LENGTH, BDAY_LENGTH, QUOTE_LENGTH);
+    
+    printf("\tRemove existing from the hash table:\n");
+    printf("\t\tremove <name of person who's entry to remove>\n\n");
+    
+    printf("\tUpdate existing entry in the hash table:\n");
+    printf("\t\tinsert <name (max %d chars)>; <bday (max %d chars)>; <quote (max %d chars)>\n\n", 
+        NAME_LENGTH, BDAY_LENGTH, QUOTE_LENGTH);
+
+    printf("\tPrint entry in the hash table:\n");
+    printf("\t\tprint <name of person who's entry to print\n\n");
+
+    printf("\tShow the hash value of a string:\n");
+    printf("\t\thash <string to hash>\n\n");
+
+    printf("\tClear the console:\n");
+    printf("\t\t cls \t\t\t or \t\t\t clear \n\n");
+
+    printf("\tExit the program:\n");
+    printf("\t\t exit \t\t\t or \t\t\t quit \n");
 }
